0596f69ed263975b570f1462a6e47c80cf496361
anurag <anurag89sharma@gmail.com>
2015-03-17 17:48:51 +0530

    added jquery

--- zimply/cart/models.py
+++ zimply/cart/models.py
@@ -7,4 +7,4 @@ class cart(models.Model):
     user = models.ForeignKey(User, blank=True, null=True)
     cart_details = ArrayField(models.CharField(max_length=200), null=True, blank=True)
     def __str__(self):  # __unicode__ on Python 2
-        return self.name.username
+        return self.user.username
